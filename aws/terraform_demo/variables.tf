fdd8d95f4e16d881d9c566f948a4ab0504c6f1f2
Sean Carolan <scarolan@gmail.com>
2018-03-28 12:01:32 -0500

    Added VPC.

--- aws/terraform_demo/variables.tf
+++ aws/terraform_demo/variables.tf
@@ -60,5 +60,5 @@ variable "subnets" {
 variable "vpc_id" {
   type = "string"
   description = "ID of the VPC to use - in case your account doesn't have default VPC"
-  default = "vpc-ec04008b"
+  default = "vpc-c45ccfbf"
 }
