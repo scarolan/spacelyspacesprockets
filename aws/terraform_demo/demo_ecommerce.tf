9359a8919bc9ea54390df61434d2b83a847ada6b
Sean Carolan <scarolan@gmail.com>
2018-03-29 13:35:56 -0500

    Added load balancer.

--- aws/terraform_demo/demo_ecommerce.tf
+++ aws/terraform_demo/demo_ecommerce.tf
@@ -105,56 +105,55 @@ resource "aws_security_group" "demo_ecommerce_dev" {
 
 # # Step 2 - Create two more machines and a load balancer
 # # Uncomment and `terraform apply` to continue the demo...
-# resource "aws_lb" "dev_lb" {
-#     name                = "ecommerce-dev-lb"
-#     internal            = false
-#     security_groups     = ["${aws_security_group.demo_ecommerce_dev.id}"]
-#     subnets             = ["${var.subnets[0]}","${var.subnets[1]}","${var.subnets[2]}"]
-#     tags                = {
-#         Name = "ecommerce_dev_lb"
-#     }
-# } 
+resource "aws_lb" "dev_lb" {
+    name                = "ecommerce-dev-lb"
+    internal            = false
+    security_groups     = ["${aws_security_group.demo_ecommerce_dev.id}"]
+    subnets             = ["${var.subnets[0]}","${var.subnets[1]}","${var.subnets[2]}"]
+    tags                = {
+        Name = "ecommerce_dev_lb"
+    }
+} 
 
 # # Create a target group
-# resource "aws_lb_target_group" "dev_tg" {
-#   name     = "ecommerce-dev-tg"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = "${var.vpc_id}"
-#   stickiness = {
-#       type = "lb_cookie"
-#       enabled = true
-#   }
-#   health_check = {
-#       interval = 30
-#       path = "/index.html"
-#       port = 80
-#       protocol = "HTTP"
-#       timeout = 5
-#       healthy_threshold = 5
-#       unhealthy_threshold = 2
-#   }
-# }
+resource "aws_lb_target_group" "dev_tg" {
+  name     = "ecommerce-dev-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+  stickiness = {
+      type = "lb_cookie"
+      enabled = true
+  }
+  health_check = {
+      interval = 30
+      path = "/index.html"
+      port = 80
+      protocol = "HTTP"
+      timeout = 5
+      healthy_threshold = 5
+      unhealthy_threshold = 2
+  }
+}
 
 # # Create a listener on port 80
-# resource "aws_lb_listener" "http_listener" {
-#   load_balancer_arn = "${aws_lb.dev_lb.arn}"
-#   port              = "80"
-#   protocol          = "HTTP"
-
-#   default_action {
-#     target_group_arn = "${aws_lb_target_group.dev_tg.arn}"
-#     type             = "forward"
-#   }
-# }
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = "${aws_lb.dev_lb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+   default_action {
+    target_group_arn = "${aws_lb_target_group.dev_tg.arn}"
+    type             = "forward"
+  }
+}
 
 # # Attach our instances to the target group
-# resource "aws_lb_target_group_attachment" "dev_tg_server" {
-#   count            = "${var.ecommerce_servers}"
-#   target_group_arn = "${aws_lb_target_group.dev_tg.arn}"
-#   target_id        = "${element(aws_instance.spacelysprocketsdev.*.id, count.index)}" 
-#   port             = 80  
-# }
+resource "aws_lb_target_group_attachment" "dev_tg_server" {
+  count            = "${var.ecommerce_servers}"
+  target_group_arn = "${aws_lb_target_group.dev_tg.arn}"
+  target_id        = "${element(aws_instance.spacelysprocketsdev.*.id, count.index)}" 
+  port             = 80  
+}
 
 # # Create a DNS record for the dev environment 
 # resource "aws_route53_record" "dev" {
