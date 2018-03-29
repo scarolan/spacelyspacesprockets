0b9d30f899ef504b6d81d682894dbe78e24e54a7
Sean Carolan <scarolan@gmail.com>
2018-03-29 15:39:48 -0500

    Remove outputs that were causing error.

--- aws/terraform_demo/outputs.tf
+++ aws/terraform_demo/outputs.tf
@@ -1,11 +1,11 @@
-output "dev_servers" {
-    value = ["${aws_instance.spacelysprocketsdev.*.public_dns}"]
-}
+#output "dev_servers" {
+#    value = ["${aws_instance.spacelysprocketsdev.*.public_dns}"]
+#}
 
-output "dev_load_bal" {
-    value = "http://${aws_lb.dev_lb.dns_name}/"
-}
+#output "dev_load_bal" {
+#    value = "http://${aws_lb.dev_lb.dns_name}/"
+#}
 
-output "dev_homepage" {
-    value = "http://${aws_route53_record.www.name}/"
-}
+#output "dev_homepage" {
+#    value = "http://${aws_route53_record.www.name}/"
+#}
