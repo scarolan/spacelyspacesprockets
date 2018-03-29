3aae38f7f53a258e35bac79103053afb81196fa7
Sean Carolan <scarolan@gmail.com>
2018-03-29 11:02:46 -0500

    Updated to install shopping cart.

--- shared/scripts/run_chef_dev.sh
+++ shared/scripts/run_chef_dev.sh
@@ -4,8 +4,8 @@ cd /tmp
 # Use this format for a public repo
 # git clone http://github.com/scarolan/projectk
 # Use this format for a private repo
-git clone http://github.com/scarolan/projectk.git
-berks install -b /tmp/projectk/shared/cookbooks/demo-ecommerce/Berksfile
-berks vendor -b /tmp/projectk/shared/cookbooks/demo-ecommerce/Berksfile
+git clone http://github.com/scarolan/spacelyspacesprockets.git
+berks install -b /tmp/spacelyspacesprockets/shared/cookbooks/demo-ecommerce/Berksfile
+berks vendor -b /tmp/spacelyspacesprockets/shared/cookbooks/demo-ecommerce/Berksfile
 mv /tmp/berks-cookbooks /tmp/cookbooks
-sudo chef-client -z -o demo-ecommerce::dev
+sudo chef-client -z -o demo-ecommerce::default
