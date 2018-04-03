62f4beed9f2419a36ead522fb385269083abaa59
Sean Carolan <scarolan@gmail.com>
2018-04-03 09:22:11 -0500

    Created new AMI with updated company name.

--- aws/terraform_demo/variables.tf
+++ aws/terraform_demo/variables.tf
@@ -14,8 +14,8 @@ variable "user" {
 
 # Packer-built AMI
 variable "demoami" {
-  default = "ami-0d65d860b98cde955"
-  description = "eCommerce Website - CentOS 7 - v0.7"
+  default = ""
+  description = "eCommerce Website - RHEL 7 - v0.8"
 }
 
 variable "key_name" {
